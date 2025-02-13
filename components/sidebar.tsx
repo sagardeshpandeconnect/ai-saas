2d5686e475e955170c17b0ca6bf2b3e3cc3d0052
Sagar Deshpande <sagar.deshpande.346@gmail.com>
2025-02-13 22:36:26 +0530

    feat(sidebar): add navigation links for desktop screens

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,63 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+import {
+  CodeIcon,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  MusicIcon,
+  SettingsIcon,
+  VideoIcon,
+} from "lucide-react";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    href: "/music",
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    icon: SettingsIcon,
+    href: "/settings",
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -11,7 +68,24 @@ const Sidebar = () => {
           <div className="relative w-8 h-8 mr-4 ">
             <Image src="/logo.png" alt="logo" layout="fill" />
           </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Genius
+          </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.label}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
