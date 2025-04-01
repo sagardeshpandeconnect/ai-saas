57fce35168273ec0b227d3946a974373c7096840
Sagar Deshpande <sagar.deshpande.346@gmail.com>
2025-04-01 23:33:16 +0530

    feat(Pro Modal Ui): complete  pro modal ui

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { MAX_FREE_COUNTS } from "@/lib/constants";
+import { Progress } from "@/components/ui/progress";
+import { Button } from "./ui/button";
+import { Zap, ZapIcon } from "lucide-react";
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -17,7 +22,27 @@ const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
     return null; // Render nothing on the server side
   }
 
-  return <div>FreeCounter</div>;
+  return (
+    <div className="px-3">
+      <Card className="bg-white/10 border-2">
+        <CardContent className="py-6">
+          <div className="text-center text-sm text-white mb-4 space-y-2">
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNTS} Free generations
+            </p>
+            <Progress
+              className="h-3"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+            />
+          </div>
+          <Button className="w-full" variant={"premium"}>
+            Upgrade
+            <ZapIcon className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default FreeCounter;
